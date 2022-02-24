c91800ebf814ab5e26ec51ae9ec380e576adde6b
IG <iikim2511@gmail.com>
2022-02-23 23:03:11 -0500

    <input className='search' placeholder='search...' onChange={(e)=>( setQuery(e.target.value.toLowerCase()))}/>

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
+import { useState } from 'react';
 
+import {Users} from './data'
 import './App.css';
 
+/* 🍀version 1. BASIC SEARCH
+.filter
+*/
+
+/* 🦄🍄
+1. input, setQuery
+
+2. query
+
+3. data_Users.filter
+
+4. if...data_Users.filter = query... map loop안에 데이터바인딩 ㄱㄱ
+*/
+
 function App() {
+
+  const [query, setQuery] = useState("");
+
+  console.log(query)
+
   return (
     <div className="App">
 
-      {/*<header className="App-header">       
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      <h1> hello</h1>
+      <h1> search filter</h1>
+
+      <input className='search' placeholder='search...' onChange={(e)=>( setQuery(e.target.value.toLowerCase()))}/>
 
       
+
+      
+      
+      <ul>
+      
+      {
+        Users.map((user)=>(
+        <li className="listItem" key={user.id}>
+                    {user.first_name}
+          </li>
+        ))
+      }
+      </ul>
+
+        
 
 
 
